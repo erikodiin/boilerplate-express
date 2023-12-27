673af22bc0fe7d8f9b8d8f1f48d0466e6edda894
Erik Odiin <erik.odiin@antonsport.no>
2023-12-27 18:11:07 +0100

    fake touppercase dotenv

--- myApp.js
+++ myApp.js
@@ -1,12 +1,13 @@
-let express = require('express');
-let app = express();
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv').config()
 
 app.get("/", (req, res) => {
   res.sendFile(`${__dirname}/views/index.html`)
 })
 
 app.get("/json", (req, res) => {
-  res.json({"message": "Hello json"})
+  res.json({"message": (process.env.MESSAGE_STYLE == "uppercase" && "HELLO JSON") || "Hello json"})
 })
 
 app.use("/public", express.static(__dirname + "/public"))
