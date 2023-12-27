93a4596674fce78c6efe4b7284704920d243ca46
Erik Odiin <erik.odiin@antonsport.no>
2023-12-27 17:05:23 +0100

    trying to add static

--- myApp.js
+++ myApp.js
@@ -1,11 +1,11 @@
 let express = require('express');
 let app = express();
 
-
 app.get("/", (req, res) => {
   res.sendFile(`${__dirname}/views/index.html`)
 })
 
+app.use("/", express.static(__dirname + "/public"))
 
 
  module.exports = app;
