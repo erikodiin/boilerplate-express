f81315e53d894ad4e411b7d7a1bbe5dbc2473a0b
Erik Odiin <erik.odiin@antonsport.no>
2023-12-27 23:01:41 +0100

    ……

--- myApp.js
+++ myApp.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const app = express();
-
 require('dotenv').config()
+
 
 app.get("/", (req, res) => {
   res.sendFile(`${__dirname}/views/index.html`)
 })
 
 app.get("/json", (req, res) => {
-  const json = { "message": "Hello json" }
-  if (process.env.MESSAGE_STYLE === "uppercase") {
+  let style = process.env.MESSAGE_STYLE
+  if (style === "uppercase") {
     res.json({ "message": "HELLO JSON" })
   } else {
     res.json({ "message": "Hello json" })
