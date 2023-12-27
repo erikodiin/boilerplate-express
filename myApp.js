4f09bd519209899436b9ea052bbfa45dc31cb1b3
Erik Odiin <erik.odiin@antonsport.no>
2023-12-27 18:17:24 +0100

    maybe now

--- myApp.js
+++ myApp.js
@@ -7,7 +7,7 @@ app.get("/", (req, res) => {
 })
 
 app.get("/json", (req, res) => {
-  res.json({"message": (process.env.MESSAGE_STYLE == "uppercase" && "HELLO JSON") || "Hello json"})
+  res.json({"message": (process.env.MESSAGE_STYLE === "uppercase" ? "HELLO JSON" : "Hello json")})
 })
 
 app.use("/public", express.static(__dirname + "/public"))
