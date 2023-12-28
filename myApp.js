ad35cd21e4f8e3e23029e1bd615420d0885f697f
Erik Odiin <erik.odiin@antonsport.no>
2023-12-28 01:27:16 +0100

    added echo route

--- myApp.js
+++ myApp.js
@@ -23,6 +23,9 @@ app.get('/now', getNow, (req, res) => {
   res.json({"time": req.time})
 })
 
+app.get('/:word/echo', (req, res) => {
+  res.json({"echo": req.params.word })
+})
 
 function logger(req, res, next) {
   console.log(`${req.method} ${__dirname+req.path} - ${req.ip}`)
